20834ad367bcb5539b9b7117641025ad8dcbd0a0
Ganpat Agarwal <ganpat.agarwal@walmart.com>
2016-10-20 12:02:38 +0530

    updating setup and adding drivers

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='netscaler-openstack',
+setup(name='netscaler_driver_openstack',
       version='0.1',
       description='Netscaler Driver for OpenStack',
-      url='https://github.com/ganpatagarwal/netscaler-openstack',
+      url='https://github.com/ganpatagarwal/netscaler_driver_openstack',
       author='Ganpat Agarwal',
       author_email='ganpat.agarwal@walmart.com',
       license='',
-      packages=['netscaler-openstack'],
+      packages=['netscaler_driver_openstack'],
       zip_safe=False)
